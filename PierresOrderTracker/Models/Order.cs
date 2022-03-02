dfe43cb96804b17b9b067412fb78fac9000728fe
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-03-02 13:14:59 -0800

    Add static list property, and GetAll / ClearAll methods to Order.cs

--- PierresOrderTracker/Models/Order.cs
+++ PierresOrderTracker/Models/Order.cs
@@ -4,17 +4,20 @@ namespace PierresOrderTracker.Models
 {
   public class Order
   {
-    public int BreadCount { get; set; }
-    public int PastryCount { get; set; }
-    // public int Id { get; }
-    // private static List<Order> _instances = new Order<Order> { };
-
-    public Order(int breadCount, int pastryCount)
+    public string OrderDescription { get; set; }
+    private static List<Order> _instances = new List<Order> {};
+    public Order(string orderDescription)
     {
-      BreadCount = breadCount;
-      PastryCount = pastryCount;
-      // _instances.Add(this);
-      // Id = _instances.Count;
+      OrderDescription = orderDescription;
+      _instances.Add(this);
     }
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    } 
   }
 }
