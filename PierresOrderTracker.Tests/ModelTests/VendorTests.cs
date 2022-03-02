228eaba8885f300b08eb8b193c6166df21dd385e
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-03-02 14:56:20 -0800

    Add GetMethod tests for Vendor properties name, address, email, phone, and description, tests pass

--- PierresOrderTracker.Tests/ModelTests/VendorTests.cs
+++ PierresOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -12,5 +12,72 @@ namespace PierresOrderTracker.Tests
       Vendor newVendor = new Vendor("name", "address", "email", "phone", "description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+    
+    [TestMethod]
+    public void GetVendorName()
+    {
+      string vendorName = "vendorName";
+      Vendor newVendor = new Vendor(vendorName, "address", "email", "phone", "description");
+
+      string result = newVendor.Name;
+
+      Assert.AreEqual(vendorName, result);
+    }
+    [TestMethod]
+    public void GetVendorAddress()
+    {
+      string vendorAddress = "vendorAddress";
+      Vendor newVendor = new Vendor("vendorName", vendorAddress, "email", "phone", "description");
+
+      string result = newVendor.Address;
+
+      Assert.AreEqual(vendorAddress, result);
+    }
+    [TestMethod]
+    public void GetVendorEmail()
+    {
+      string vendorEmail = "vendorEmail";
+      Vendor newVendor = new Vendor("vendorName", "vendorAddress", vendorEmail, "phone", "description");
+
+      string result = newVendor.Email;
+
+      Assert.AreEqual(vendorEmail, result);
+    }
+    [TestMethod]
+    public void GetVendorPhone()
+    {
+      string vendorPhone = "vendorPhone";
+      Vendor newVendor = new Vendor("vendorName", "vendorAddress", "vendorEmail", vendorPhone, "description");
+
+      string result = newVendor.Phone;
+
+      Assert.AreEqual(vendorPhone, result);
+    }
+    [TestMethod]
+    public void GetVendorDescription()
+    {
+      string vendorDescription = "vendorDescription";
+      Vendor newVendor = new Vendor("vendorName", "vendorAddress", "vendorEmail", "vendorPhone", vendorDescription);
+
+      string result = newVendor.Description;
+
+      Assert.AreEqual(vendorDescription, result);
+    }
+
+    // [TestMethod]
+    // public void FindVendor()
+    // {
+    //   //Arrange
+    //   string name01 = "Billy Baker";
+    //   string name02 = "Callies Cookies";
+    //   Vendor newVendor1 = new Vendor(name01);
+    //   Vendor newVendor2 = new Vendor(name02);
+
+    //   //Act
+    //   Vendor result = Vendor.Find(2);
+
+    //   //Assert
+    //   Assert.AreEqual(newVendor2, result);
+    // }
   }
 }
