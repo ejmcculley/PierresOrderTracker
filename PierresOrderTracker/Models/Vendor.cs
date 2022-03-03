933a310388b12ce6ea301719014cc47507c4cbb6
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-03-02 16:28:59 -0800

    AddOrder method added to Vendor.cs

--- PierresOrderTracker/Models/Vendor.cs
+++ PierresOrderTracker/Models/Vendor.cs
@@ -32,5 +32,13 @@ namespace PierresOrderTracker.Models
    {
      return _instances;
    }
+   public static Vendor Find(int searchId)
+   {
+     return _instances[searchId-1];
+   }
+   public void AddOrder(Order orderDescription)
+   {
+     Orders.Add(orderDescription);
+   }
   }
 }
